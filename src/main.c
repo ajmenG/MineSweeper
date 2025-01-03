65191737bd1f4d5f2d0b1365530da71c480c405c
Cyprian Ciesielski <83280609+CyprianCiesielski@users.noreply.github.com>
2025-01-03 18:29:34 +0000

    problemy z getopt

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include "../include/board.h"
+#include "../include/game.h"
 #include "../include/parse_file.h"
 #include <bits/getopt_core.h>
 
@@ -15,40 +16,52 @@ void print_usage(const char *prog_name)
 
 int main(int argc, char *argv[])
 {
+    int opt;
 
-    if (argc == 1)
+    int rows, cols, difficulty, mines;
+    printf("Podaj poziom trudności (1-3): ");
+    scanf("%d", &difficulty);
+
+    switch (difficulty)
     {
-        int rows, cols, difficulty, mines;
-        printf("Podaj poziom trudności (1-3): ");
-        scanf("%d", &difficulty);
+        case 1:
+            rows = 9;
+            cols = 9;
+            mines = 10;
+            break;
+        case 2:
+            rows = 16;
+            cols = 16;
+            mines = 40;
+            break;
+        case 3:
+            rows = 16;
+            cols = 30;
+            mines = 99;
+            break;
+    }   
 
-        switch (difficulty)
-        {
-            case 1:
-                rows = 9;
-                cols = 9;
-                mines = 10;
-                break;
-            case 2:
-                rows = 16;
-                cols = 16;
-                mines = 40;
-                break;
-            case 3:
-                rows = 16;
-                cols = 30;
-                mines = 99;
-                break;
-        }   
+    if (difficulty < 1 || difficulty > 3)
+    {
+        printf("Niepoprawny poziom trudności\n");
+        return EXIT_FAILURE;
+    }
 
-        if (difficulty < 1 || difficulty > 3)
+    Board *board = board_create(rows, cols, difficulty, mines);
+    print_board(board);
+        
+    while ((opt = getopt(argc, argv, "f:r:"))){
+        switch (opt)
         {
-            printf("Niepoprawny poziom trudności\n");
-            return EXIT_FAILURE;
+            case 'f':
+                flag_field(board, 6, 6);
+                print_board(board);
+                break;
+            case 'r':
+                reveal_field(board, 8, 8);
+                print_board(board);
+                break;
         }
-
-        Board *board = board_create(rows, cols, difficulty, mines);
-        print_board(board);
     }
 
     return 0;
